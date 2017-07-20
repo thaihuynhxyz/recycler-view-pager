eee5a88091221c8bbb22dcac3e6f67f6ef3e5f9f
th <thai.huynh.xyz@gmail.com>
2017-07-20 13:25:21 +0700

    add recyclerViewFragment

--- app/src/main/java/com/th/viewpager/RecyclerViewFragment.java
+++ app/src/main/java/com/th/viewpager/RecyclerViewFragment.java
@@ -3,10 +3,12 @@ package com.th.viewpager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class RecyclerViewFragment extends Fragment {
 
@@ -25,5 +27,25 @@ public class RecyclerViewFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
 
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(new RecyclerView.Adapter() {
+
+            @Override
+
+            public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                return new RecyclerView.ViewHolder(new TextView(parent.getContext())) {
+                };
+            }
+
+            @Override
+            public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+                ((TextView) holder.itemView).setText(String.format(getString(R.string.recycler_item), position));
+            }
+
+            @Override
+            public int getItemCount() {
+                return 100;
+            }
+        });
     }
 }
